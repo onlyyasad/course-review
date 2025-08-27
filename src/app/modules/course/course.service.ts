4485d3a482e0882288d814a14604306d0aa31e03
hafij <hafij.alasad23@gmail.com>
2025-08-27 23:50:26 +0600

    feat: min price, max price filter added

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -3,22 +3,40 @@ import { TCourse } from './course.interface'
 import { Course } from './course.model'
 
 const getAllCourseFromDB = async (query: Record<string, unknown>) => {
-  const page = parseInt(query.page as string) || 1
-  const limit = parseInt(query.limit as string) || 10
+  const {
+    minPrice,
+    maxPrice,
+    tags,
+    startDate,
+    endDate,
+    language,
+    provider,
+    durationInWeeks,
+    level,
+    sortBy,
+    sortOrder,
+    page: pageNumber,
+    limit: perPage,
+  } = query
+
+  const queryFilter: Record<string, unknown> = {}
+
+  const page = parseInt(pageNumber as string) || 1
+  const limit = parseInt(perPage as string) || 10
   const skip = (page - 1) * limit
 
   let sort = '-createdAt'
 
-  if (query.sortBy) {
-    const sortBy = (query.sortBy as string).split(',')
-    const filterSort = sortBy.filter((singleSort) =>
+  if (sortBy) {
+    const sortByArray = (sortBy as string).split(',')
+    const filterSort = sortByArray.filter((singleSort) =>
       courseSortableFields.includes(singleSort),
     )
     const sortWithOrder = filterSort.length
       ? filterSort.map((singleSort) => {
-          if (query.sortOrder) {
-            const sortOrder = query.sortOrder === 'desc' ? '-' : ''
-            return `${sortOrder + singleSort}`
+          if (sortOrder) {
+            const sortByOrder = sortOrder === 'desc' ? '-' : ''
+            return `${sortByOrder + singleSort}`
           }
           return singleSort
         })
@@ -26,7 +44,19 @@ const getAllCourseFromDB = async (query: Record<string, unknown>) => {
     sort = sortWithOrder.length ? sortWithOrder.join(' ') : '-createdAt'
   }
 
-  const result = await Course.find().skip(skip).limit(limit).sort(sort)
+  if (minPrice || maxPrice) {
+    if (minPrice) {
+      queryFilter.price = { $gte: Number(minPrice) }
+    }
+    if (maxPrice) {
+      queryFilter.price = { $lte: Number(maxPrice) }
+    }
+  }
+
+  const result = await Course.find(queryFilter)
+    .skip(skip)
+    .limit(limit)
+    .sort(sort)
   const count = await Course.countDocuments()
 
   const data = {
